5cb43e63adee8a3eaf01a7e0995ead725a89234a
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-16 11:58:22 -0600

    Extended RuntimeException for BootpackException

--- src/Exceptions/BootpackException.php
+++ src/Exceptions/BootpackException.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aaroncadrian
- * Date: 1/16/18
- * Time: 11:44 AM
- */
 
 namespace AaronAdrian\Bootpack\Exceptions;
 
 
-class BootpackException
+class BootpackException extends \RuntimeException
 {
-
+    //
 }
