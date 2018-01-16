1d9a1c7f4a14c29dee62cdcba365cbf0a6bd23fc
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-16 11:21:52 -0600

    Created a service class for creating packages

---
+++ src/Services/CreatePackageService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaroncadrian
+ * Date: 1/16/18
+ * Time: 11:18 AM
+ */
+
+namespace AaronAdrian\Bootpack\Services;
+
+
+class CreatePackageService
+{
+
+}
