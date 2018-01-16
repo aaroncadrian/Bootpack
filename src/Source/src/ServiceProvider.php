8eff3fc4553c870a6c4313d4ac918dd716234517
Aaron Adrian <aaroncadrian@gmail.com>
2018-01-15 23:27:03 -0600

    Fixed path issues in ServiceProvider stub

--- src/Source/src/ServiceProvider.php
+++ src/Source/src/ServiceProvider.php
@@ -17,27 +17,27 @@ class {{ UCNAME }}ServiceProvider extends ServiceProvider
         // $router->aliasMiddleware('{{ NAME }}', \{{ NAMESPACE }}\Http\Middleware\{{ UCNAME }}Middleware::class);
 
         $this->publishes([
-            __DIR__.'/publishable/config/{{ NAME }}.php' => config_path('{{ NAME }}.php'),
+            __DIR__.'/../publishable/config/{{ NAME }}.php' => config_path('{{ NAME }}.php'),
         ], '{{ NAME }}_config');
 
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        $this->loadMigrationsFrom(__DIR__ . '/publishable/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../publishable/migrations');
 
-        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', '{{ NAME }}');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', '{{ NAME }}');
 
         $this->publishes([
-            __DIR__ . '/resources/lang' => resource_path('lang/vendor/{{ NAME }}'),
+            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/{{ NAME }}'),
         ]);
 
-        $this->loadViewsFrom(__DIR__ . '/resources/views', '{{ NAME }}');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', '{{ NAME }}');
 
         $this->publishes([
-            __DIR__ . '/resources/views' => resource_path('views/vendor/{{ NAME }}'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/{{ NAME }}'),
         ]);
 
         $this->publishes([
-            __DIR__ . '/resources/assets' => public_path('vendor/{{ NAME }}'),
+            __DIR__ . '/../resources/assets' => public_path('vendor/{{ NAME }}'),
         ], '{{ NAME }}_assets');
 
         if ($this->app->runningInConsole()) {
@@ -55,7 +55,7 @@ class {{ UCNAME }}ServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/publishable/config/{{ NAME }}.php', '{{ NAME }}'
+            __DIR__ . '/../publishable/config/{{ NAME }}.php', '{{ NAME }}'
         );
     }
 }
